52b00ada1d978f077cc3178953000143d269dd8b
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2017-11-14 15:38:16 +0000

    Display instance resources in summaries

--- app/Groot/App/List/Instance.hs
+++ app/Groot/App/List/Instance.hs
@@ -11,7 +11,8 @@ import Control.Lens
 import Data.Conduit
 import qualified Data.Conduit.List as CL
 import Data.Data
-import Data.Text
+import Data.List
+import qualified Data.Text as T
 import GHC.Generics
 import Text.PrettyPrint.Tabulate
 import Network.AWS
@@ -21,12 +22,42 @@ import Groot.App.List.Base
 import Groot.Core
 import Groot.Data
 
+data ResourceType =
+    Memory
+  | CPU
+  deriving (Eq, Show, Generic, Data)
+
+data ResourceSummary = ResourceSummary
+  { resourceType :: ResourceType
+  , allocated    :: Int
+  , available    :: Int
+  } deriving (Eq, Generic, Data)
+
+instance Show ResourceSummary where
+  show (ResourceSummary _ alloc avail) = concat [show avail, "/", show alloc]
+
+instance CellValueFormatter ResourceSummary
+
+resourceSummary :: ECS.ContainerInstance -> ResourceType -> Maybe ResourceSummary
+resourceSummary inst resType = (ResourceSummary resType) <$> rAlloc <*> rAvail
+  where resName = case resType of
+          Memory -> "MEMORY"
+          CPU    -> "CPU"
+        
+        findResource :: [ECS.Resource] -> Maybe Int
+        findResource rs = (view ECS.rIntegerValue) =<< find (\x -> maybe False (== resName) $ x ^. ECS.rName) rs
+        
+        rAlloc = findResource $ inst ^. ECS.ciRegisteredResources
+        rAvail = findResource $ inst ^. ECS.ciRemainingResources
+
 data InstanceSummary = InstanceSummary
   { ec2Id           :: String
   , arn             :: String
   , status          :: String
   , runningTasks    :: Int
   , pendingTasks    :: Int
+  , memory          :: ResourceSummary
+  , cpu             :: ResourceSummary
   , ecsAgentVersion :: String
   , dockerVersion   :: String
   } deriving (Eq, Show, Generic, Data)
@@ -34,14 +65,16 @@ data InstanceSummary = InstanceSummary
 instance Tabulate InstanceSummary
 
 instance HasSummary ECS.ContainerInstance InstanceSummary where
-  summarize inst = InstanceSummary <$> iId <*> iArn <*> iStatus <*> iRunning <*> iPending <*> iAgentV <*> iDockerV
-    where iId      = unpack <$> inst ^. ECS.ciEc2InstanceId
-          iArn     = unpack <$> inst ^. ECS.ciContainerInstanceARN
-          iStatus  = unpack <$> inst ^. ECS.ciStatus
+  summarize inst = InstanceSummary <$> iId <*> iArn <*> iStatus <*> iRunning <*> iPending <*> iMem <*> iCpu <*> iAgentV <*> iDockerV
+    where iId      = T.unpack <$> inst ^. ECS.ciEc2InstanceId
+          iArn     = T.unpack <$> inst ^. ECS.ciContainerInstanceARN
+          iStatus  = T.unpack <$> inst ^. ECS.ciStatus
           iRunning = inst ^. ECS.ciRunningTasksCount
           iPending = inst ^. ECS.ciPendingTasksCount
-          iAgentV  = unpack <$> (inst ^. ECS.ciVersionInfo >>= view ECS.viAgentVersion)
-          iDockerV = unpack <$> (inst ^. ECS.ciVersionInfo >>= view ECS.viDockerVersion)
+          iMem     = resourceSummary inst Memory
+          iCpu     = resourceSummary inst CPU
+          iAgentV  = T.unpack <$> (inst ^. ECS.ciVersionInfo >>= view ECS.viAgentVersion)
+          iDockerV = T.unpack <$> (inst ^. ECS.ciVersionInfo >>= view ECS.viDockerVersion)
 
 summarizeInstances :: Maybe ClusterRef -> AWS [InstanceSummary]
 summarizeInstances cId = 
