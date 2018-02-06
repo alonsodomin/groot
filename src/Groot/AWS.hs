e1e9c1208ee468eff86efd55594dffd035cc75f8
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2018-02-06 09:44:37 +0000

    Move AWS error handlers to the AWS module

--- src/Groot/AWS.hs
+++ src/Groot/AWS.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Groot.AWS
      (
        module Groot.AWS.Cluster
@@ -5,10 +7,38 @@ module Groot.AWS
      , module Groot.AWS.Service
      , module Groot.AWS.Task
      , module Groot.AWS.TaskDef
+     -- Error handlers
+     , handleHttpException
+     , handleServiceError
      ) where
+
+import           Control.Lens
+import           Data.Semigroup            ((<>))
+import qualified Data.Text                 as T
+import           Network.AWS
+import           Network.HTTP.Conduit
+import           Network.HTTP.Types.Status
 
 import           Groot.AWS.Cluster
 import           Groot.AWS.Instance
 import           Groot.AWS.Service
 import           Groot.AWS.Task
 import           Groot.AWS.TaskDef
+import           Groot.Console
+import           Groot.Data.Text
+
+-- AWS Error handlers
+
+handleHttpException :: MonadConsole m => HttpException -> m ()
+handleHttpException (InvalidUrlException url reason) =
+  putError $ "Url " <> (styled yellowStyle $ toText url) <> " is invalid due to: " <> (styled redStyle $ toText reason)
+handleHttpException (HttpExceptionRequest req _) =
+  putError $ "Could not communicate with " <> (styled yellowStyle $ toText . host $ req) <> "."
+
+handleServiceError :: MonadConsole m => ServiceError -> m ()
+handleServiceError err =
+  let servName  = toText $ err ^. serviceAbbrev
+      statusMsg = toText . statusMessage $ err ^. serviceStatus
+      message   = maybe "" toText $ err ^. serviceMessage
+      styledSt  = styled yellowStyle (T.concat [servName, " ", statusMsg])
+  in putError $ styledSt <+> (styleless message)
--- src/Groot/Core.hs
+++ src/Groot/Core.hs
@@ -1,42 +1,11 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RankNTypes        #-}
-
 module Groot.Core
      (
        module Groot.AWS
      , module Groot.Core.Common
-     -- Error handlers
-     , handleHttpException
-     , handleServiceError
      ) where
 
-import           Control.Lens
-import           Data.Semigroup            ((<>))
-import qualified Data.Text                 as T
-import           Network.AWS
-import           Network.HTTP.Conduit
-import           Network.HTTP.Types.Status
-
 import           Groot.AWS
-import           Groot.Console
 import           Groot.Core.Common
-import           Groot.Data.Text
-
--- AWS Error handlers
-
-handleHttpException :: MonadConsole m => HttpException -> m ()
-handleHttpException (InvalidUrlException url reason) =
-  putError $ "Url " <> (styled yellowStyle $ toText url) <> " is invalid due to: " <> (styled redStyle $ toText reason)
-handleHttpException (HttpExceptionRequest req _) =
-  putError $ "Could not communicate with " <> (styled yellowStyle $ toText . host $ req) <> "."
-
-handleServiceError :: MonadConsole m => ServiceError -> m ()
-handleServiceError err =
-  let servName  = toText $ err ^. serviceAbbrev
-      statusMsg = toText . statusMessage $ err ^. serviceStatus
-      message   = maybe "" toText $ err ^. serviceMessage
-      styledSt  = styled yellowStyle (T.concat [servName, " ", statusMsg])
-  in putError $ styledSt <+> (styleless message)
 
 -- Tasks
 
