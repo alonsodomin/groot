88000838a721ebda88ce8b4e2b2bfdcc26a71445
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2018-04-06 16:22:03 +0100

    Remove static flag from build

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,33 @@
+import Distribution.Package
+import Distribution.PackageDescription (PackageDescription)
+import qualified Distribution.PackageDescription as Pckg
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.Setup
+import Distribution.Simple.LocalBuildInfo
+
+customPostBuild :: Args -> BuildFlags -> PackageDescription -> LocalBuildInfo -> IO ()
+customPostBuild _ _ pckg _ = do
+  putStrLn "So this thing has been built"
+  generateRpmSpecFile pckg "bla"
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { postBuild = customPostBuild }
+
+-- RPM
+
+data RpmSpec = RpmSpec
+  { rpmName :: String
+  , rpmVersion :: String
+  , rpmSummary :: String
+  , rpmDescription :: String
+  } deriving (Eq, Show)
+
+generateRpmSpecFile :: PackageDescription -> FilePath -> IO ()
+generateRpmSpecFile pckg _ = print generateSpec
+  where generateSpec = RpmSpec {
+        rpmName = unPackageName . pkgName . Pckg.package $ pckg
+      , rpmVersion = showVersion . pkgVersion . Pckg.package $ pckg
+      , rpmSummary = Pckg.synopsis pckg
+      , rpmDescription = Pckg.description pckg
+    }
