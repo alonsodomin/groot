142fa6b0a5a28993cd52c384dae402e94a83bd6a
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2018-04-09 11:51:30 +0100

    This is the actual fix for the hackage script

--- scripts/travis/hackage.sh
+++ scripts/travis/hackage.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-if [[ ! -z "TRAVIS_TAG" ] && [ "TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ ! -z "TRAVIS_TAG" ]] && [[ "TRAVIS_OS_NAME" == "linux" ]]; then
   echo "Generating source distribution package and uploading it to Hackage..."
   echo "{ \"username\": \"$HACKAGE_USERNAME\", \"password\": \"$HACKAGE_PASSWORD\" }" > "$HOME/.stack/upload/credentials.json"
   stack sdist
   stack upload
+else
+  echo "Skipping Hackage release because this is not a tagged build."
 fi
