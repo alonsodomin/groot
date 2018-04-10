796c941b8c6e4014e0dc083ba55ac1328cca22b0
Antonio Alonso Dominguez <alonso.domin@gmail.com>
2018-04-10 20:59:20 +0100

    Use variables in the hackage.sh script

--- scripts/travis/hackage.sh
+++ scripts/travis/hackage.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ ! -z "TRAVIS_TAG" ]] && [[ "TRAVIS_OS_NAME" == "linux" ]]; then
+if [[ ! -z "$TRAVIS_TAG" ]] && [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   echo "Generating source distribution package and uploading it to Hackage..."
   echo "{ \"username\": \"$HACKAGE_USERNAME\", \"password\": \"$HACKAGE_PASSWORD\" }" > "$HOME/.stack/upload/credentials.json"
   stack sdist
