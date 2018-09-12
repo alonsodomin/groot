de59ed38223a6628b9f4fba153e09dbdb4d99291
Antonio Alonso Dominguez <antoniodominguez@rentalcars.com>
2018-09-12 12:44:55 +0100

    No need for a free monad in there, a RWS just does the trick

--- test/Test/Groot/Console.hs
+++ test/Test/Groot/Console.hs
@@ -10,89 +10,89 @@ module Test.Groot.Console
      ( describeConsole
      ) where
 
-import           Control.Monad.Free
-import           Control.Monad.Identity
-import           Control.Monad.Reader
-import           Control.Monad.State
-import           Data.Semigroup           ((<>))
+import           Control.Applicative
+import           Control.Monad.RWS.Lazy
 import           Data.Text                (Text)
 import           Test.Hspec
 
 import           Groot.Console
 import           Groot.Internal.Data.Text
 
-data ConsoleOp a =
-    PutMessage Severity Text a
-  | AskUser Text (Maybe Text -> a)
-  deriving Functor
-
-type ConsoleM = Free ConsoleOp
-
-instance {-# OVERLAPS #-} MonadConsole ConsoleM where
-  putMessage sev txt = liftF (PutMessage sev (toText txt) ())
-  askUser txt        = liftF (AskUser (toText txt) id)
-
 data ConsoleEvent =
     MessagePut Severity Text
   | UserAnswered Text (Maybe Text)
   deriving (Eq, Show)
 
-type ConsoleState = ReaderT (Maybe Text) (State [ConsoleEvent])
+type Msg = (Severity, Text)
+type MockConsole = RWS (Maybe Text) [Msg] [ConsoleEvent]
 
-runConsoleM :: Maybe Text -> ConsoleM a -> (a, [ConsoleEvent])
-runConsoleM input console =
-  runReader (accumEvents console) input
-  where accumEvents :: ConsoleM a -> Reader (Maybe Text) (a, [ConsoleEvent])
-        accumEvents console =
-          mapReaderT (\st -> Identity $ runState st []) (interpretConsole console)
+instance {-# OVERLAPS #-} MonadConsole MockConsole where
+  askUser prompt = do
+    answer <- ask
+    modify (\prev -> (UserAnswered (toText prompt) answer):prev)
+    return answer
+  putMessage sev msg = do
+    let txt = toText msg
+    modify (\prev -> (MessagePut sev txt):prev)
+    tell [(sev, txt)]
 
-        interpretConsole :: ConsoleM a -> ConsoleState a
-        interpretConsole = foldFree $ \msg -> do
-          prev <- get
-          case msg of
-            PutMessage severity text next -> do
-              let event = MessagePut severity text
-              put (event:prev)
-              return next
-            AskUser txt fn -> do
-              answer <- ask
-              let event = UserAnswered txt answer
-              put (event:prev)
-              return (fn answer)
+writtenMessages :: MockConsole () -> [Msg]
+writtenMessages console = snd $ evalRWS console Nothing []
 
-eventsAfterAnswer :: Text -> ConsoleM a -> [ConsoleEvent]
-eventsAfterAnswer answer console = snd $ runConsoleM (Just answer) console
+evalConsole :: Maybe Text -> MockConsole a -> (a, [ConsoleEvent])
+evalConsole input console =
+  let (res, evts, _) = runRWS console input []
+  in (res, reverse evts)
 
-events :: ConsoleM a -> [ConsoleEvent]
-events console = snd $ runConsoleM Nothing console
+events :: Maybe Text -> MockConsole a -> [ConsoleEvent]
+events input console = snd $ evalConsole input console
+
+userAnswered :: Text -> MockConsole a -> Maybe Text
+userAnswered answer console = grabAnswer $ events (Just answer) console
+  where grabAnswer :: [ConsoleEvent] -> Maybe Text
+        grabAnswer =
+          let getAnswer (UserAnswered _ ans) = ans
+              getAnswer _                    = Nothing
+          in (foldl (<|>) Nothing) . (fmap getAnswer)
 
 describeConsole :: IO ()
 describeConsole = hspec $ do
   describe "putXXX" $ do
     it "is equivalent to a putMessage with the severity enum" $ do
       let txt = toText ("hello" :: String)
-      events (putSuccess txt) `shouldBe` [MessagePut Success txt]
-      events (putInfo    txt) `shouldBe` [MessagePut Info    txt]
-      events (putWarn    txt) `shouldBe` [MessagePut Warn    txt]
-      events (putError   txt) `shouldBe` [MessagePut Error   txt]
-      events (putDebug   txt) `shouldBe` [MessagePut Debug   txt]
+      let console = do
+            putSuccess txt
+            putInfo    txt
+            putWarn    txt
+            putError   txt
+            putDebug   txt
+      (writtenMessages console) `shouldBe` [
+          (Success, txt)
+        , (Info,    txt)
+        , (Warn,    txt)
+        , (Error,   txt)
+        , (Debug,   txt)
+        ]
+
   describe "askUser" $ do
     it "should yield an AskUser event" $ do
       let txt = toText ("hello" :: String)
-      eventsAfterAnswer "bye" (askUser txt) `shouldBe` [UserAnswered txt (Just "bye")]
+      userAnswered "bye" (askUser txt) `shouldBe` (Just "bye")
+
   describe "askUserYN" $ do
     it "should interpret no answer as the default" $ do
       let prompt = toText ("say something" :: String)
       let falseExpectedPrompt = prompt <> " [yN] "
       let trueExpectedPrompt = prompt <> " [Yn] "
-      runConsoleM Nothing (askUserYN False prompt) `shouldBe` (False, [UserAnswered falseExpectedPrompt Nothing])
-      runConsoleM Nothing (askUserYN True  prompt) `shouldBe` (True,  [UserAnswered trueExpectedPrompt  Nothing])
+      evalConsole Nothing (askUserYN False prompt) `shouldBe` (False, [UserAnswered falseExpectedPrompt Nothing])
+      evalConsole Nothing (askUserYN True  prompt) `shouldBe` (True,  [UserAnswered trueExpectedPrompt  Nothing])
+
     it "should translate a y answer to a true" $ do
       let prompt = toText ("foo" :: String)
       let expectedPrompt = prompt <> " [yN] "
-      runConsoleM (Just "Y") (askUserYN False prompt) `shouldBe` (True, [UserAnswered expectedPrompt (Just "Y")])
+      evalConsole (Just "Y") (askUserYN False prompt) `shouldBe` (True, [UserAnswered expectedPrompt (Just "Y")])
     it "should translate a n answer to a false" $ do
       let prompt = toText ("foo" :: String)
       let expectedPrompt = prompt <> " [Yn] "
-      runConsoleM (Just "N") (askUserYN True prompt) `shouldBe` (False, [UserAnswered expectedPrompt (Just "N")])
+      evalConsole (Just "N") (askUserYN True prompt) `shouldBe` (False, [UserAnswered expectedPrompt (Just "N")])
 
