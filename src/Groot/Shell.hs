cf337fd3dfb1c9567db7e398279671b03d78b162
Antonio Dominguez <antonio.dominguez@wejo.com>
2019-11-01 10:03:06 +0000

    Improved shell experience, with history

--- src/Groot/Shell.hs
+++ src/Groot/Shell.hs
@@ -6,14 +6,19 @@ import           Control.Monad.IO.Class
 import           Data.String
 import qualified Data.Text                as T
 import           Data.Version             (showVersion)
-import           Options.Applicative
+import           Options.Applicative      hiding (defaultPrefs)
+import qualified Options.Applicative      as Opts
 import           Options.Applicative.Help (parserHelp)
 import           Paths_groot              (version)
+import           System.Console.Haskeline hiding (defaultPrefs, defaultSettings,
+                                           display)
+import qualified System.Console.Haskeline as Line
+import           System.Directory         (createDirectoryIfMissing,
+                                           getHomeDirectory)
 import           System.Exit              (exitSuccess)
-import           System.Posix.Signals
 
 import           Groot.CLI
-import           Groot.Console            (askUser, display)
+import           Groot.Console            (display)
 import           Groot.Core
 import           Groot.Internal.Data.Text
 
@@ -31,59 +36,60 @@ grootShellCommand = (DefaultCmd <$> grootCommand) <|> hsubparser
   <> command "exit" (info (pure ExitCmd) (progDesc "Exits the shell"))
   )
 
-shellPrefs = defaultPrefs
+shellParserPrefs :: ParserPrefs
+shellParserPrefs = Opts.defaultPrefs
+
+shellParserInfo :: ParserInfo GrootShellCmd
+shellParserInfo = info grootShellCommand mempty
+
+loadShellSettings :: IO (Settings IO)
+loadShellSettings = do
+  userHome <- getHomeDirectory
+  let homeFolder = userHome ++ "/.groot"
+  createDirectoryIfMissing True homeFolder
+  return $ Line.defaultSettings { historyFile = Just $ homeFolder ++ "/history" }
 
 execShellCmd :: GrootShellCmd -> GrootIO ()
 execShellCmd (DefaultCmd cmd) = execGrootCmd cmd
 execShellCmd HelpCmd =
-  let help = parserHelp shellPrefs grootShellCommand
-  in liftIO $ print help
+  let helpDesc = parserHelp shellParserPrefs grootShellCommand
+  in liftIO $ print helpDesc
 execShellCmd ExitCmd = do
   liftIO $ putStrLn "Bye!"
   liftIO exitSuccess
 execShellCmd VersionCmd =
   liftIO . putStrLn $ showVersion version
 
-shellInfo :: ParserInfo GrootShellCmd
-shellInfo = info grootShellCommand mempty
-
-prompt :: StyledText
-prompt = styled blueStyle "groot>"
-
+welcomeMsg :: StyledText
 welcomeMsg = (styleless "Welcome to the Groot Shell" <+> (styleless . T.pack $ showVersion version) <> (styleless "."))
   </> ("Type '" <> (styled yellowStyle "help") <> "' for a list of available commands.")
 
 grootShell :: Env -> IO ()
 grootShell env = do
-  initShell
-  loop
+  display welcomeMsg
+  settings <- loadShellSettings
+  runInputT settings loop
 
   where
-    initShell :: IO ()
-    initShell = do
-      installHandler keyboardSignal interruptHandler Nothing
-      display welcomeMsg
-
-    loop :: IO ()
-    loop = do
-      line <- askUser (prompt <> styleless " ")
-      case (T.unpack <$> line) of
-        Just cmd -> runCommand $ words cmd
-        Nothing  -> pure ()
+    loop :: InputT IO ()
+    loop = withInterrupt $ do
+      mline <- getInputLine "groot> "
+      case mline of
+        Just ""  -> return ()
+        Just cmd -> handle cancelledHandler (runCommand $ words cmd)
+        Nothing  -> return ()
       loop
 
-    runCommand :: [String] -> IO ()
-    runCommand args = do
-      result <- pure $ execParserPure shellPrefs shellInfo args
+    runCommand :: [String] -> InputT IO ()
+    runCommand args = liftIO $ do
+      result <- pure $ execParserPure shellParserPrefs shellParserInfo args
       case result of
         Success cmd -> runGrootT (execShellCmd cmd) env
         Failure err -> do
-          (help, _, _) <- pure $ execFailure err ""
-          liftIO $ print help
-        CompletionInvoked result -> do
-          str <- liftIO $ execCompletion result ""
+          (helpDesc, _, _) <- pure $ execFailure err ""
+          liftIO $ print helpDesc
+        CompletionInvoked res -> do
+          str <- liftIO $ execCompletion res ""
           liftIO $ putStrLn str
 
-    interruptHandler = CatchOnce $ do
-      putStrLn ""
-      loop
+    cancelledHandler Interrupt = outputStrLn "Cancelled."
